39a58b743e813547eb4eb64a7d5c5655d9550354
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-04-05 09:46:35 -0300

    chore: arquivos de apoio

---
+++ src/utils/texts.js
@@ -0,0 +1,16 @@
+export const about = {
+  title: 'paragrafos do about',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fug.',
+  text2: 'Somos especialistas em seres humanos, fazemos do corte de cabelo, da barba bem feita, ou qual serviço for, um momento especial, com música boa, ambiente agradável e se quiser um bom papo. Somos a ART CLUB.'
+}
+
+export const Jaffar = {
+  text: `O encanto de Rafael Jaffar com cabelos começou cedo. Ele, que nasceu em ambiente de Salão, começou cedo a desenvolver interesse pela área, sendo a quarta geração de profissionais da família. Aos 20 anos de idade mergulhou nesse mundo e começou a trabalhar e desenvolver suas técnicas com cursos e pesquisas.
+  Aos 24, participando da ProfissionalFair na sua primeira competição, conquistou o terceiro colocado.`,
+
+  text2: 'E o qual surgiu nele a vontade de buscar mais conhecimento ainda, então decidiu fazer mais um curso ainda, um de peso, Pivot Point, um curso com renomeação Internacional, o qual segundo ele, ganhou 5anos de experiência em 4dias de curso intensivo. Rafael também participou da produção de materiais para jornal e entrevistas.',
+
+  text3: 'Com muito estudo e pesquisa, participou não só de treinamentos de profissionais do mercado, workshops e cursos, como também, ajudou marcas a desenvolverem produtos de ponta para profissionais e também para consumidor final. Teve uma grande experiência com a AlfaParf, foi um dos AlfasTeam de Belo Horizonte, formação em 2018.',
+
+  text4: 'Os anos com a AlfaParf mudaram os seus comportamentos como profissional, e que nunca vai esquecer o tempo que passou com eles. Hoje ele é proprietário da Barbearia Art Club. E diz que sua missão ainda está só começando.'
+}
