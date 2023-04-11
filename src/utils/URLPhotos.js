4ebb3d8984da0e17ff4ff7b0ee62f5ab8c746b9c
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-04-11 16:08:03 -0300

    chore: lib de swipe e urls de fotos

---
+++ src/utils/URLPhotos.js
@@ -0,0 +1,14 @@
+export const carrousel = [
+  {
+    url: 'https://live.staticflickr.com/65535/52807246581_05d2e0473e_b.jpg'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52807656060_f3e4c45ab4_b.jpg'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52809671915_8b040251bd_b.jpg'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52807656205_ecb7bc206a_b.jpg'
+  }
+]
