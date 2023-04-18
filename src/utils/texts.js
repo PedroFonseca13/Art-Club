6b39f0652ae7028e3416ea10bf8699c9e35498f0
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-04-18 10:39:04 -0300

    chore: altera texto

--- src/utils/texts.js
+++ src/utils/texts.js
@@ -13,9 +13,9 @@ export const Jaffar = {
   text: `O encanto de Rafael Jaffar com cabelos começou cedo. Ele, que nasceu em ambiente de Salão, começou cedo a desenvolver interesse pela área, sendo a quarta geração de profissionais da família. Aos 20 anos de idade mergulhou nesse mundo e começou a trabalhar e desenvolver suas técnicas com cursos e pesquisas.
   Aos 24, participando da ProfissionalFair na sua primeira competição, conquistou o terceiro colocado.`,
 
-  text2: 'E o qual surgiu nele a vontade de buscar mais conhecimento ainda, então decidiu fazer mais um curso ainda, um de peso, Pivot Point, um curso com renomeação Internacional, o qual segundo ele, ganhou 5anos de experiência em 4dias de curso intensivo. Rafael também participou da produção de materiais para jornal e entrevistas.',
+  text2: 'E o qual surgiu nele a vontade de buscar mais conhecimento ainda, então decidiu fazer mais um curso ainda, um de peso, Pivot Point, um curso com renomeação Internacional, o qual segundo ele, ganhou 5anos de experiência em 4 dias de curso intensivo. Rafael também participou da produção de materiais para jornal e entrevistas.',
 
-  text3: 'Com muito estudo e pesquisa, participou não só de treinamentos de profissionais do mercado, workshops e cursos, como também, ajudou marcas a desenvolverem produtos de ponta para profissionais e também para consumidor final. Teve uma grande experiência com a AlfaParf, foi um dos AlfasTeam de Belo Horizonte, formação em 2018.',
+  text3: 'Com muito estudo e pesquisa, participou não só de treinamentos de profissionais do mercado, workshops e cursos, como também, ajudou marcas a desenvolverem produtos de ponta para profissionais e também para consumidor final. Teve uma grande experiência com a AlfaParf, foi um dos AlfasTeam de Belo Horizonte, formação em 2019.',
 
   text4: 'Os anos com a AlfaParf mudaram os seus comportamentos como profissional, e que nunca vai esquecer o tempo que passou com eles. Hoje ele é proprietário da Barbearia Art Club. E diz que sua missão ainda está só começando.'
 }
